065feeaa5c30be3e9c4d33c326136468efa21777
UdayTayade18 <udaytayde245@gmil.com>
2025-02-19 12:12:52 +0530

    Add Initial file

---
+++ Ifelse/IfelseDemo.java
@@ -0,0 +1,9 @@
+class IfelseDemo {
+    public static void main(String[]argss){
+        int age=19;
+        if(age<=18){
+            System.out.println("Eligible for votting ");
+        }
+    }
+    
+}
