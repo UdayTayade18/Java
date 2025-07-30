40180534ee2fbd9444f9e9402fdd8c083368d917
UdayTayade18 <udaytayde245@gmil.com>
2025-07-30 08:05:56 +0530

    data type revison

---
+++ Java Revision/Data Taype/data_type.java
@@ -0,0 +1,9 @@
+class data_type {
+    public static void main(String args[]){
+        byte age =127;
+        System.out.println(age);
+        byte age2=128;
+        System.out.println(age2);
+    }
+    
+}
