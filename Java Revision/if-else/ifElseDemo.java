f159bcd7ea7b198e768faab8185177940561957b
UdayTayade18 <udaytayde245@gmil.com>
2025-08-01 08:00:12 +0530

    if else revision code added

---
+++ Java Revision/if-else/ifElseDemo.java
@@ -0,0 +1,12 @@
+class ifElseDemo {
+    public static void main(String[]argg){
+
+        int age=22;
+        if(age>=18){
+            System.out.println("Eligible for vote");
+        }else{
+            System.out.println("Not eligible for vote");
+        }
+    }
+    
+}
