d00042cb3b214c990038057991ee0c3bc412ce77
UdayTayade18 <udaytayde245@gmil.com>
2025-08-01 07:48:25 +0530

    new data taype code added

--- Java Revision/Data Taype/data_type.java
+++ Java Revision/Data Taype/data_type.java
@@ -2,8 +2,23 @@ class data_type {
     public static void main(String args[]){
         byte age =127;
         System.out.println(age);
-        byte age2=128;
-        System.out.println(age2);
+
+        /*byte age2=128;         //incompatible types: possible lossy conversion from int to byte
+        System.out.println(age2);*/
+
+        float pt=7.8f;
+        //float pt2=8.9;    // consider as double.
+        System.out.println(pt);
+
+        short s=10;
+        System.out.println(s); //range of short is : -32768 to 32767
+
+        int x=378494;
+        System.out.println(x);
+
+
+        char wing = 'A';
+        System.out.println(wing);
     }
     
 }
