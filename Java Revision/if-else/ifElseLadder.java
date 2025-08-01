788db4e6b7f18c279dc75bdf659b6bbd1ad4e77b
UdayTayade18 <udaytayde245@gmil.com>
2025-08-01 08:07:50 +0530

    if-else ladder code added

---
+++ Java Revision/if-else/ifElseLadder.java
@@ -0,0 +1,13 @@
+class ifElseLadder {
+    public static void main(String []argss){
+        int num = -9;
+        if(num>1){
+            System.out.println(num+" It is a positive number ");
+        }else if(num<1){
+            System.out.println(num+" It is a negative number");
+        }else{
+            System.out.println("Number is Zero");
+        }
+    }
+    
+}
