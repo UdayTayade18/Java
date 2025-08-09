db79cd1885319c802574d8ee74f17d8528f340cd
UdayTayade18 <udaytayde245@gmil.com>
2025-08-09 18:32:30 +0530

    Input-output code added

---
+++ Input-output/program2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class program2 {
+    public static void main(String[]aegs){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Num");
+        int num=sc.nextInt();
+        System.out.println("Number is "+num);
+    }
+    
+}
---
+++ Input-output/program3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class program3 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter Student Name");
+        String name=sc.next();
+
+        System.out.println("Enter college Name");
+        String colName=sc.next();
+
+        System.out.println("Enter Student Id");
+        int id=sc.nextInt();
+
+        System.out.println("Enter marks");
+        float marks=sc.nextFloat();
+
+        System.out.println(name);
+        System.out.println(colName);
+        System.out.println(id);
+        System.out.println(marks);
+    }
+    
+}
